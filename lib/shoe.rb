1ab028e61f9322e7bd16eb20869cf4a5d90454ae
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-06 13:02:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,9 +5,9 @@ class Shoe
     @brand = brand
   end
   
-  def cobble
+  def cobble(condition)
     puts "Your shoe is as good as new!"
-    shoe.condition = "new"
+    @condition = condition
   end
   
   
