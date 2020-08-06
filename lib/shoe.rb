b36b082c533b7d20ad01d74b344035770040fb5b
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-06 13:01:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,7 @@ class Shoe
   
   def cobble
     puts "Your shoe is as good as new!"
+    shoe.condition = "new"
   end
   
   
@@ -15,4 +16,13 @@ end
 
 
 
-shoe.condition = "new"
+# shoe.condition = "new"
+
+
+#     it 'makes the shoe\'s condition new' do
+#       shoe.condition = "old"
+#       shoe.cobble
+#       expect(shoe.condition).to eq("new")
+#     end
+#   end
+end
